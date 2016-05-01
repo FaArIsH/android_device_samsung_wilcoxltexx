7a32d37b129b8a128532760b1866bd945b09631f
FaArIsH <faarishhassan@gmail.com>
2016-05-01 11:57:15 +0500

    Add lt02ltespr

--- cm.mk
+++ cm.mk
@@ -4,9 +4,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 #$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-$(call inherit-product, device/samsung/wilcoxltexx/full_wilcoxltexx.mk)
+$(call inherit-product, device/samsung/lt02ltespr/full_lt02ltespr.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=wilcoxltexx TARGET_DEVICE=wilcoxlte BUILD_FINGERPRINT="samsung/wilcoxltexx/wilcoxlte:4.4.2/KOT49H/G3815XXUCNH1:user/release-keys" PRIVATE_BUILD_DESC="wilcoxltexx-user 4.4.2 KOT49H G3815XXUCNH1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lt02ltespr TARGET_DEVICE=lt02lte BUILD_FINGERPRINT="samsung/lt02ltespr/lt02lte:4.4.2/KOT49H/G3815XXUCNH1:user/release-keys" PRIVATE_BUILD_DESC="lt02ltespr-user 4.4.2 KOT49H G3815XXUCNH1 release-keys"
 
-PRODUCT_DEVICE := wilcoxltexx
-PRODUCT_NAME := cm_wilcoxltexx
+PRODUCT_DEVICE := lt02ltespr
+PRODUCT_NAME := cm_lt02ltespr
