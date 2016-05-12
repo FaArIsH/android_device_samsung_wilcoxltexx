c89501b88d9bcf3c3fb921f476c95d458e315e61
FaArIsH <faarishhassan@gmail.com>
2016-05-12 13:45:02 +0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/lt02ltespr/full_lt02ltespr.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lt02ltespr TARGET_DEVICE=lt02lte BUILD_FINGERPRINT="samsung/lt02ltespr/lt02lte:4.4.2/KOT49H/G3815XXUCNH1:user/release-keys" PRIVATE_BUILD_DESC="lt02ltespr-user 4.4.2 KOT49H G3815XXUCNH1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lt02ltespr TARGET_DEVICE=lt02lte BUILD_FINGERPRINT="samsung/lt02ltespr/lt02ltespr:4.4.2/KOT49H/T217SVPUBOH1:user/release-keys" PRIVATE_BUILD_DESC="lt02ltespr-user 4.4.2 KOT49H T217SVPUBOH1 release-keys"
 
 PRODUCT_DEVICE := lt02ltespr
 PRODUCT_NAME := cm_lt02ltespr
